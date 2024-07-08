6a2faab19e02f48ee84e43d1ca867989d57a9d1d
鷹取敏志 <takatorix@gmail.com>
2024-07-08 17:27:10 +0900

    vespaへのデータフィールドまで実装

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
+building-search-app-w-ml-vespa
+===
 
-https://docs.vespa.ai/en/tutorials/news-1-getting-started.html
 
-```
-$ docker exec vespa /opt/vespa/bin/vespa status deploy --wait 300
-
-$ 
-```# building-search-app-w-ml-vespa
---
+++ feed-master-db/feed_dump_to_vespa.py
@@ -0,0 +1,41 @@
+#!usr/bin/env python
+import bz2
+from vespa.application import Vespa
+
+
+def generate_bulk_buffer():
+    buf = []
+    with bz2.open("dataset/simplewiki-202109-pages-with-pageviews-20211001.bz2", "rt") as bz2f:
+        for line in bz2f:
+            id, title, text, pageviews = line.rstrip().split("\t")
+            buf.append(
+                {
+                    "id": id,
+                    "fields": {
+                        "title": title,
+                        "text": text,
+                        "pageviews": pageviews
+                    }
+                }
+            )
+            if 500 <= len(buf):
+                yield buf
+                buf.clear()
+    if buf:
+        yield buf
+        
+
+client = Vespa(url = 'http://vespa', port = 8080)
+
+def callback(response, id):
+    if not response.is_successful():
+        print(
+            f"Failed to feed document {id} with status code {response.status_code}: Reason {response.get_json()}"
+        )
+    
+for buf in generate_bulk_buffer():
+    client.feed_iterable(buf, schema = "simplewiki", callback=callback)
+
+
+    
+    
