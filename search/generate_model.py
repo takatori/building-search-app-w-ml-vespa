01e91df0d03b17e9e0c25e74fb0d4b46009f1bc7
鷹取敏志 <takatorix@gmail.com>
2024-07-25 23:22:26 +0900

    model生成

---
+++ search/generate_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import json
+
+from xgboost import DMatrix, train
+
+
+feature_names = [
+    "queryTermCount",
+    "nativeRank",
+    "pageviews",
+    "vespa.summaryFeatures.cached",
+]
+
+
+# XGBoost学習パラメータ
+params = {
+    "objective": "rank:pairwise",  # LambdaMARTによるランク学習
+    "eval_metric": "ndcg",  # nDCGの評価値を学習ログに出力
+    "tree_method": "hist",
+    "grow_policy": "lossguide",
+    "max_leaves": 60,
+    "subsample": 0.45,
+}
+
+# 訓練データセット
+training_input = DMatrix(
+    "tmp/hands_on_featuredata.txt.training", feature_names=feature_names
+)
+
+# 検証データセット
+validation_input = DMatrix(
+    "tmp/hands_on_featuredata.txt.validation", feature_names=feature_names
+)
+
+# モデルの学習
+evals = [(training_input, "train"), (validation_input, "valid")]
+bst = train(
+    params,
+    training_input,
+    num_boost_round=1000,
+    evals=evals,
+    # early_stopping_rounds=50,
+    verbose_eval=10,
+)
+
+# モデルの出力
+model = bst.get_dump(dump_format="json")
+
+
+with open("tmp/hands_on_model.json", "w") as f:
+    json.dump(model, f)
