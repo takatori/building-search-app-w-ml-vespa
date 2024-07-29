05ee9f0fcdf94759e93e4af7d02209eeef689f16
鷹取敏志 <takatorix@gmail.com>
2024-07-29 23:26:23 +0900

    vespa-cliのインストールを戻す

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,6 +10,13 @@ poetry install
 
 echo "SETUP Vespa"
 
+VESPA_CLI_VERSION=8.367.14
+curl -L -o .bin/vespa-cli.tar.gz https://github.com/vespa-engine/vespa/releases/download/v${VESPA_CLI_VERSION}/vespa-cli_${VESPA_CLI_VERSION}_linux_amd64.tar.gz
+tar -xzvf .bin/vespa-cli.tar.gz -C .bin
+mv .bin/vespa-cli_${VESPA_CLI_VERSION}_linux_amd64/bin/vespa .bin/vespa
+rm .bin/vespa-cli.tar.gz
+rm -rf .bin/vespa-cli_${VESPA_CLI_VERSION}_linux_amd64
+
 vespa config set target http://vespa:19071
 vespa status
 vespa deploy vespa-config --wait 30
